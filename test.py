9cbe207f43a4ba19bbccd7e1911fda23bf3b5737
Fadeev Mikhail <m.fadeew@gmail.com>
2021-03-02 11:45:19 +0300

    save

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ with open("calibration_data", "rb") as calibration_data:
     calibration.retval, calibration.cameraMatrix, calibration.distCoeffs, calibration.rvecs, calibration.tvecs = pickle.load(
         calibration_data)
 
-img = cv2.imread('Photos/500_0_0.jpg')
+img = cv2.imread('Photos/0002.jpg')
 plt.imshow(img)
 # plt.show()
 
@@ -35,19 +35,35 @@ plt.legend()
 # plt.savefig('test.png', dpi=300)
 plt.show()
 
+# x = np.array([
+#     # 9     11       5
+#     [1.17,  -1.22,  1.83],
+#     [-1.57, -0.91, 1.93],
+#     [0,     0,        0]
+# ])
+
 C = np.array([
-markers[11][1][0][0],
-markers[7][1][0][0],
-markers[5][1][0][0]
+markers[9][1][0][0],
+markers[23][1][0][0],
+markers[17][1][0][0]
 ])
 
 x = np.array([
-    [-1, -1,  1.5],
-    [-1, 1.5, 1.5],
-    [0,   0,   0]
+    # 9     23       17
+    [1.17,  1.88,  0.66],
+    [-1.57, 0.33,    0 ],
+    [0,     0,       0 ]
 ])
 
-A = (C**-1).dot(x)
+a1 = [1.17, -1.57, 0]
+a2 = [1.88, 0.33, 0]
+a3 = [0.66, 0, 0]
+
+p1 = markers[9][1][0][0]
+p2 = markers[23][1][0][0]
+p3 = markers[17][1][0][0]
+
+A = np.linalg.inv(C).dot(x)
 
 print(C)
 print(x)
